392ec35135228d07ce9b072389c3d7fd6e1810ac
chris98122 <41262973+chris98122@users.noreply.github.com>
2019-04-19 17:22:47 +0800

    user和admin不同界面

--- src/cart.java
+++ src/cart.java
@@ -4,32 +4,35 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import javax.servlet.http.HttpSession;
+import java.util.*;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
-@WebServlet("/login")
-public class login  extends HttpServlet {
+
+@WebServlet("/cart_show")
+public class cart  extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public login() {
+    public cart() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -51,48 +54,26 @@ public class login  extends HttpServlet {
             response.setCharacterEncoding("UTF-8");
             request.setCharacterEncoding("UTF-8");
             PrintWriter out = response.getWriter();
+            HttpSession session = request.getSession();
+            int userid = (int) session.getAttribute("userid");
 
-            String qname = request.getParameter("name");
-            String password=request.getParameter("password");
-
-            Map<String, String> loginUserMap = new HashMap<>();
-
-            String hql = "FROM UserEntity where name =:n";
+            String hql = "from OrdersEntity o " +
+                    " where o.user.id =:uid and o.isCart=1";
             Query query = HibernateUtil.getSessionFactory()
-                    .getCurrentSession().createQuery(hql).setString("n",qname);
+                    .getCurrentSession().createQuery(hql).setInteger("uid", userid);
+            List< OrdersEntity > result = query.list();
 
-
-            List<UserEntity> result = query.list();
-            if(result.get(0).getPassword().equals(password)) {
-                HttpSession session = request.getSession();
-                int userid=result.get(0).getId();
-                String username=result.get(0).getName();
-                session.setAttribute("username", username);
-                session.setAttribute("userid", userid);
-                if(userid == 1)
-                {
-                    session.setAttribute("isAdmin",true);
-                    out.write("管理员登录成功");
-                }
-                else
-                {
-                    session.setAttribute("isAdmin",false);
-                    out.write("用户登录成功");
-                }
-            }
+            String re= JSON.toJSONString(result, true);
+            out.write(re);
             out.close();
             HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
-            if ( ServletException.class.isInstance( ex ) ) {
-                throw ( ServletException ) ex;
-            }
-            else {
-                throw new ServletException( ex );
+            if (ServletException.class.isInstance(ex)) {
+                throw (ServletException) ex;
+            } else {
+                throw new ServletException(ex);
             }
         }
     }
-
-
 }
